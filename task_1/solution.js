800b93dbac03ea7fecc22723dea6342a11b59f8a
amarchenko2882 <112930309+amarchenko2882@users.noreply.github.com>
2022-09-16 17:53:25 +0700

    Update solution.js

--- task_1/solution.js
+++ task_1/solution.js
@@ -3,25 +3,25 @@ function addInCartMessage(title, price) {
     let productPrice = price;
 
     // Задание №1.1. Оповещение о добавлении в корзину
-    let message;
-    // создайте переменную message
-    message = productName;
-    // присвойте переменной message значение переменной productName
-    message = message + " ";
-    // добавьте в конец переменной message пробел
-    message = message + "за";
-    // добавьте в конец переменной message текст 'за'
-    message = message + " ";
-    // добавьте в конец переменной message пробел
-    message = message + productPrice;
-    // добавьте в конец переменной message значение переменной productPrice
-    message = message + " ";
-    // добавьте в конец переменной message пробел
-    message = message + "теперь в корзине!";
-    // добавьте в конец переменной message текст 'теперь в корзине!'
-    console.log (message); 
-    // выведите переменную message в консоль
     
+    // создайте переменную message
+    let message;
+    // присвойте переменной message значение переменной productName
+    message = 'productName';
+    // добавьте в конец переменной message пробел
+    message = message + " ";
+    // добавьте в конец переменной message текст 'за'
+    message = message + "за";
+    // добавьте в конец переменной message пробел
+    message = message + " ";
+    // добавьте в конец переменной message значение переменной productPrice
+    message = message + productPrice;
+    // добавьте в конец переменной message пробел
+    message = message + " ";
+    // добавьте в конец переменной message текст 'теперь в корзине!'
+    message = message + "теперь в корзине!";
+    // выведите переменную message в консоль
+    console.log(message); 
     // Конец решения задания №1.1.
 
     return message;
