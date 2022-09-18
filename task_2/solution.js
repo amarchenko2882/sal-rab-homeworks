e7706377df9ff57c8e0bb7735ca48401455ea8ef
amarchenko2882 <112930309+amarchenko2882@users.noreply.github.com>
2022-09-18 22:53:56 +0700

    Update solution.js

--- task_2/solution.js
+++ task_2/solution.js
@@ -8,15 +8,15 @@ function calcShipping(sum, min, shipping) {
     // создайте переменную shippingSum
     let shippingSum;
     // если productsSum равно 0,
-    if productsSum = 0;
+    if productsSum >= 0;
     // то shippingSum присвоить значение 0
-    
+    let shippingSum === 0;
     // если productsSum Больше или равна freeShippingMinSum,
     // то shippingSum присвоить значение 0
-
+    let freeShipping = shippingSum === 0;
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
-
+     
     // Конец решения задания №2.1.
 
     return shippingSum;
